bb4fe689e1b7b5fa89c7fab040210d7db1f82637
VISWA <20MH1A04H9@acoe.edu.in>
2025-03-23 20:25:58 +0530

    Update marks.sh

--- marks.sh
+++ marks.sh
@@ -3,7 +3,7 @@ read -p "what is your score? " result
 if [ $result -ge 100 ]
 then 
     echo "You are Expert"
-elif [ $result -lt 100 ]
+elif [ $result -lt 100 ] && [ $result -gt 60 ]
 then
     echo "Congrats!"
 else
