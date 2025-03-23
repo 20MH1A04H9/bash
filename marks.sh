cc6b0a9d1a14f692e6e45523864d8cfba8ab6095
VISWA <20MH1A04H9@acoe.edu.in>
2025-03-23 19:36:52 +0530

    marks

--- marks.sh
+++ marks.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+read -p "what is your score? " result
+if [ $result -ge 100 ]
+then 
+    echo "You are Expert"
+elif [ $result -lt 100 ]
+then
+    echo "Congrats!"
+else
+    echo "Sorry ! You Failed"
+fi
