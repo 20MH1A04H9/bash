aac87c69b1843733cdbeef293f90bba2bd8a2075
VISWA <20MH1A04H9@acoe.edu.in>
2025-03-23 20:39:32 +0530

    hashgen
    update , but cut the useless

--- md5.sh
+++ md5.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 read -p  " Enter the hash value: " hash
 echo "The hash value is: $hash"
-echo "The hash value is: " echo -n $hash | md5sum
+echo "The hash value is: " echo -n $hash | md5sum | cut -d " " -f 1
