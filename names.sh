b8b3d9f5dafcf8e90754dbd698d6425dd7a74902
VISWA <20MH1A04H9@acoe.edu.in>
2025-03-25 10:33:41 +0530

    local , gobal

---
+++ names.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name1="vishnu"
+name2="vicky"
+
+change(){
+    local name1="viswanadh"
+    echo "In function: $name1 , $name2"
+    name2="sai"
+}
+echo "Before function call : $name1, $name2"
+change
+echo "After function call : $name1, $name2"
