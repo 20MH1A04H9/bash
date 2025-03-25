f414662718c91b53cf2327986c548c9e920ab239
VISWA <20MH1A04H9@acoe.edu.in>
2025-03-25 15:00:07 +0530

    python3

--- Python Scripting/name.py
+++ Python Scripting/name.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
-name="viswa"
-age=21
-print("my name is " + name + " and my age is " + str(age) + " years")
+name="viswa" #string
+age=21 #integer
+gpa=6.8 #float
+print("my name is " + name + " and my age is " + str(age) + " years." " My CGPA is " + str(gpa) + " points")
---
+++ Python Scripting/test.py
@@ -0,0 +1,4 @@
+a=10
+b=20
+ans=a+b
+print(" The result is " + str(ans))
