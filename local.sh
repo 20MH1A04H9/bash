ecc82280aa4312de9265e9a72630aef277b7ae20
VISWA <20MH1A04H9@acoe.edu.in>
2025-03-25 10:34:50 +0530

    local

--- local.sh
+++ local.sh
@@ -6,7 +6,7 @@ name2="vicky"
 change(){
     local name1="viswanadh"
     echo "In function: $name1 , $name2"
-    name2="sai"
+    local name2="sai"
 }
 echo "Before function call : $name1, $name2"
 change
